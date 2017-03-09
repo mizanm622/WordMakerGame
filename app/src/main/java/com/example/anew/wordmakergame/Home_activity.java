41e8d77c227faad9c141232542fc71ae30ee41e6
mks25524 <mks25524withme@gmail.com>
2017-03-09 21:49:40 +0600

    first commit

---
+++ app/src/main/java/com/example/anew/wordmakergame/Home_activity.java
@@ -0,0 +1,13 @@
+package com.example.anew.wordmakergame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Home_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_activity);
+    }
+}
