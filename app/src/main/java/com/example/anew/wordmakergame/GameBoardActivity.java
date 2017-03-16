84327f9658661ac7ae6b393e0997259fc6c215f3
mizanm622 <mizanm622@gmail.com>
2017-03-16 18:52:29 +0600

    game board

---
+++ app/src/main/java/com/example/anew/wordmakergame/GameBoardActivity.java
@@ -0,0 +1,13 @@
+package com.example.anew.wordmakergame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameBoardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_board);
+    }
+}
--- app/src/main/java/com/example/anew/wordmakergame/Home_activity.java
+++ app/src/main/java/com/example/anew/wordmakergame/Home_activity.java
@@ -1,13 +1,24 @@
 package com.example.anew.wordmakergame;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Home_activity extends AppCompatActivity {
-
+Button b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_activity);
+        b1=(Button)findViewById(R.id.button1);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in=new Intent(Home_activity.this, GameBoardActivity.class);
+                startActivity(in);
+            }
+        });
     }
 }
