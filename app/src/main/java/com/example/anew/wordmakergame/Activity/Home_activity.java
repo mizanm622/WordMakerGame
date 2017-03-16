510a9aef72d81c600f132bcfcb0968041ca17467
mizanm622 <mizanm622@gmail.com>
2017-03-16 19:14:22 +0600

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/build.gradle
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/res/layout/activity_home_activity.xml
    #	app/src/main/res/values/strings.xml
    #	app/src/main/res/values/styles.xml

--- app/src/main/java/com/example/anew/wordmakergame/Activity/Home_activity.java
+++ app/src/main/java/com/example/anew/wordmakergame/Activity/Home_activity.java
@@ -1,10 +1,12 @@
-package com.example.anew.wordmakergame;
+package com.example.anew.wordmakergame.Activity;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import com.example.anew.wordmakergame.R;
 
 public class Home_activity extends AppCompatActivity {
 Button b1;
